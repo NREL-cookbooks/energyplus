9b27cc9facedfb94c8422a0224acf4524a3ddf7a
Nicholas Long <nicholas.long@nrel.gov>
2013-03-25 21:45:29 -0600

    upgrading to version 7.2

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,22 +8,23 @@
 if platform_family?("debian")
   remote_file "#{Chef::Config[:file_cache_path]}/EnergyPlus-#{node[:energyplus][:version]}-Linux-64.tar.gz" do
     source "http://developer.nrel.gov/downloads/buildings/EnergyPlus-#{node[:energyplus][:version]}-Linux-64.tar.gz"
-    mode 00755
     #checksum node[:energyplus][:checksum]
+    mode 00755
+
+    action :create_if_missing
   end
 
   # only works for 64 bit machines right now because it has to move lib directories:  x86_64-linux
   bash "install_energyplus" do
-    #password = ftxeDhpZ2
     cwd Chef::Config[:file_cache_path]
 
     #use the version that is in the vagrant directory for now
     code <<-EOH
       tar xzf EnergyPlus-#{node[:energyplus][:version]}-Linux-64.tar.gz
-      mv EnergyPlus-7-1-0 /usr/local/
+      mv EnergyPlus-#{node[:energyplus][:version].gsub(".","-")} /usr/local/
 
       cd /usr/local/bin
-      find ../EnergyPlus-7-1-0/bin/ -type f -perm -o+rx -exec ln -s {} \\;
+      find ../EnergyPlus-#{node[:energyplus][:version].gsub(".","-")}/bin/ -type f -perm -o+rx -exec ln -s {} \\;
     EOH
 
     not_if { ::File.exists?("/usr/local/bin/energyplus") }
